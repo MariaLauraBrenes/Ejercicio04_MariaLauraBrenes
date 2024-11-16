f72c1766894f1967d044536ead5af0b910aa7b0c
MariaLauraBrenes <mbrenes20078@ufide.ac.cr>
2024-11-16 13:50:33 -0600

    Primer Commit

---
+++ Ejercicio04_MariaLauraBrenes/src/main/java/Ejercicio4MariaLauraBrenes/com/Ejercicio4MariaLauraBrenesApplication.java
@@ -0,0 +1,13 @@
+package Ejercicio4MariaLauraBrenes.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio4MariaLauraBrenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio4MariaLauraBrenesApplication.class, args);
+	}
+
+}
---
+++ Ejercicio04_MariaLauraBrenes/src/test/java/Ejercicio4MariaLauraBrenes/com/Ejercicio4MariaLauraBrenesApplicationTests.java
@@ -0,0 +1,13 @@
+package Ejercicio4MariaLauraBrenes.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio4MariaLauraBrenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
